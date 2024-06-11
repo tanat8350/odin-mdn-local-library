b7ae653fc56432bf52b98913ad9de89f4fc0245b
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-06-12 04:23:54 +0700

    init controllers

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const coolRouter = require('./routes/cool');
+const catalogRouter = require('./routes/catalog');
 
 const app = express();
 
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cool', coolRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
